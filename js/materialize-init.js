44f0dddb23fbaec749dfedd8cffc423bf3f8d5fa
Federico Ramírez <00009220@uca.edu.sv>
2022-07-09 00:28:47 -0600

    Safe On Sivar Web Page 1.0

---
+++ js/materialize-init.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('.scrollspy').scrollSpy();
+});
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+$(document).ready(function () {
+    $('.slider').slider();
+});
+
+$(document).ready(function () {
+    $('.carousel').carousel({
+        indicators: true,
+        dist: -50,
+        numVisible: 7
+    });
+});
